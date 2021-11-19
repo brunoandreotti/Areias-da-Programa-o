7246bf255c7dcdc0e488506de7980b16ae51f57b
Bruno Andreotti <brunoandreotti@outlook.com>
2021-11-19 13:40:43 -0300

    Fase-1-personagem-1

--- assets/scripts/sel-perso.js
+++ assets/scripts/sel-perso.js
@@ -8,27 +8,15 @@ button.addEventListener('click', selecionarPersonagem)
 
 // FUNCTIONS
 
-// function redirecionarParaFase(numEscolha) {
-//   if (numEscolha == 1) {
-//     window.location.href = './personagens/personagem-1/fase-1.html'
-//   } else if (numEscolha == 2) {
-//     window.location.href = './personagens/personagem-2/fase-1.html'
-//   } else {
-//     window.location.href = './personagens/personagem-3/fase-1.html'
-//   }
-// }
-
-
 function redirecionarParaFase(numEscolha) {
   if (numEscolha == 1) {
-    window.location.assign('./personagens/personagem-1/fase-1.html')
+    window.location.assign('./personagens/personagem-1/fase-1/fase-1.html')
   } else if (numEscolha == 2) {
     window.location.assign('./personagens/personagem-2/fase-1.html')
   } else {
     window.location.assign('./personagens/personagem-3/fase-1.html')
   }
 }
-
 
 function selecionarPersonagem(event) {
   event.preventDefault()
---
+++ personagens/personagem-1/fase-2/fase-2.js
@@ -0,0 +1,42 @@
+// VARIBLES
+
+var button = document.querySelector('#button')
+
+// EVENTS
+
+button.addEventListener('click', selecionarPersonagem)
+
+// FUNCTIONS
+
+function redirecionarParaFase(numEscolha) {
+  if (numEscolha == 1) {
+    window.location.assign('./game-over.html')
+  } else if (numEscolha == 2) {
+    window.location.assign('../fase-2/index.html')
+  }
+}
+
+function selecionarPersonagem(event) {
+  event.preventDefault()
+  var escolha = parseInt(
+    prompt(`Faça sua escolha:
+  1 - Seguir seu caminho de acordo com o mapa.
+  2 - Ir até a possível miragem.
+  
+  Obs: Selecione baseado no número da escolha!`)
+  )
+
+  if (escolha == 1) {
+    alert(
+      'Você seguiu seu trajeto e mesmo com o mapa não encontrou seu destino e se perdeu no deserto.'
+    )
+    redirecionarParaFase(escolha)
+  } else if (escolha == 2) {
+    alert(
+      'Você vai até a figura misteriosa, chegando lá percebe que se trata de um dos Senhores da Programação, O Ferreiro do HTML.'
+    )
+    redirecionarParaFase(escolha)
+  } else {
+    alert('Selecione uma alternativa válida!')
+  }
+}
