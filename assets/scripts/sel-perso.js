854f9b3d033c472ee9cadfa96b3de0b939f16f20
Bruno Andreotti <77709788+brunoandreotti@users.noreply.github.com>
2021-11-24 14:24:00 -0300

    Update sel-perso.js

--- assets/scripts/sel-perso.js
+++ assets/scripts/sel-perso.js
@@ -12,9 +12,9 @@ function redirecionarParaFase(numEscolha) {
   if (numEscolha == 1) {
     window.location.assign('./personagens/personagem-1/fase-1/fase-1.html')
   } else if (numEscolha == 2) {
-    window.location.assign('../../personagens/personagem-2/fase-1/html/fase-1.html')
+    window.location.assign('./personagens/personagem-2/fase-1/html/fase-1.html')
   } else {
-    window.location.assign('../../personagens/personagem-3/fase-1/html/fase-1.html')
+    window.location.assign('./personagens/personagem-3/fase-1/html/fase-1.html')
   }
 }
 
